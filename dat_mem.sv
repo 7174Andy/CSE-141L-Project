f990b260f8e2b82cc5fd4e284c977d5497fe9e22
7174Andy <hep003@ucsd.edu>
2024-12-14 12:54:42 -0800

    modify data_mem

--- dat_mem.sv
+++ dat_mem.sv
@@ -6,15 +6,15 @@ module dat_mem (
   input[7:0] addr,		      // address pointer
   output logic[7:0] dat_out);
 
-  logic[7:0] core[256];       // 2-dim array  8 wide  256 deep
+  logic[7:0] mem_core[256];       // 2-dim array  8 wide  256 deep
 
 // reads are combinational; no enable or clock required
-  assign dat_out = core[addr];
+  assign dat_out = mem_core[addr];
 
 // writes are sequential (clocked) -- occur on stores or pushes 
   always_ff @(posedge clk)
     if(wr_en) begin				  // wr_en usually = 0; = 1 		
-      core[addr] <= dat_in;
+      mem_core[addr] <= dat_in;
       $display("[%0t] WRITE: addr = 0x%0h, dat_in = 0x%0h", $time, addr, dat_in);
     end
     else begin
