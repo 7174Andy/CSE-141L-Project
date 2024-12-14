f30bf857e24f044447869286528dea13834e49ff
7174Andy <hep003@ucsd.edu>
2024-12-14 15:01:47 -0800

    test udpate

--- PC_LUT.sv
+++ PC_LUT.sv
@@ -1,5 +1,5 @@
 module PC_LUT #(parameter D=8)(
-  input       [5:0] idx,    // target 32 values
+  input       [ 2:0] idx,    // target 4 values
   input       branch,
   output logic[D-1:0] target);
 
@@ -7,15 +7,9 @@ module PC_LUT #(parameter D=8)(
 
   if (branch) begin
     case(idx)
-        // Only doing absolute branch, meaning that, we are not adding target value to the current PC. We update the PC
-        0: target = 17;   // NEGATIVE
-        1: target = 22;   // FIND_MSB
-        2: target = 24;   // FIND_BIT_LOOP
-        3: target = 31;   // HANDLE_ZERO
-        4: target = 35;   // NORMALIZE
-        5: target = 44;   // SHIFT
-        6: target = 46;   // STORE_RESULT
-        7: target = 64;   // HALT  
+      // Only doing absolute branch, meaning that, we are not adding target value to the current PC. We update the PC
+      0: target = 4;    // BRANCH_TAKEN
+      1: target = 8;    // END
     endcase
   end
   else begin
